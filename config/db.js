59d6a30d9dc15da2f4a9cec0a5e2841c3479d221
mdp <mdp@yahoo-inc.com>
2014-07-03 22:37:38 -0700

    Change db api

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 var mongo = require('mongoskin');
-exports.db = mongo.db(process.env["MONGO_URL"] || "mongodb://localhost:27017/righttobeunforgotten", {native_parser:true});
+module.exports = mongo.db(process.env["MONGO_URL"] || "mongodb://localhost:27017/righttobeunforgotten", {native_parser:true});
 
