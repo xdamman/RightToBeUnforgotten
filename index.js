817d57c3b9eafeb907f515bada831698e071ac48
Xavier Damman <xdamman@gmail.com>
2014-07-03 21:37:56 -0700

    Fix typos

--- index.js
+++ index.js
@@ -7,7 +7,3 @@ require('./config/express')(server);
 console.log("Server listening on port " + server.set('port') + ' in ' + server.set('env') + ' environment');
 server.listen(server.set('port'));
 
-app.get('/', function(req, res) {
-  res.send('Now with less forgetting');
-});
-
